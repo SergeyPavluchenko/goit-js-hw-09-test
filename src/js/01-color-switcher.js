3c3505cd4acbe2add1cceaf4e7f216e2586876aa
Sergey <s.pavluchenko90@gmail.com>
2023-12-24 00:42:42 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+console.log(body);
 
+btnStart.addEventListener('click', btnStartColor);
+btnStop.addEventListener('click', btnStopColor);
+
+function btnStartColor() {
+  //   btnStart.setAttribute('disabled', true);
+  //   btnStop.removeAttribute('disabled');
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  changeColorBody = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, 1000);
+}
+
+function btnStopColor() {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(changeColorBody);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
