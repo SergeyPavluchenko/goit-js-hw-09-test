cce3b47528bbed5ef0bf35721bae45dbc86383a2
SergeyPavluchenko <102330212+SergeyPavluchenko@users.noreply.github.com>
2024-01-16 21:29:03 +0000

    Deploying to gh-pages from @ SergeyPavluchenko/goit-js-hw-09-test@6e1299884bd29929564901dfd373cca5b0b44aa1 🚀

---
+++ 01-color-switcher.0b1512b3.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");console.log(o),t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,changeColorBody=setInterval((function(){o.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(changeColorBody)}))}();
+//# sourceMappingURL=01-color-switcher.0b1512b3.js.map
